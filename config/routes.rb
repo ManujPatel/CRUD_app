0449aa885ef1075a462c0baf80e437fcb2df1855
Manuj Patel <manuj.p@simformsolutions.com>
2022-01-17 13:10:46 +0530

    Added Couse and enrollment model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  #get 'enrollments/index'
+  #get 'enrollments/new'
+  #get 'enrollments/create'
+  #get 'enrollments/destroy'
+  #get 'courses/index'
   #get 'students/index'
   #get 'students/new'
   #get 'students/create'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_16_165636) do
+ActiveRecord::Schema.define(version: 2022_01_17_050814) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "enrollments", force: :cascade do |t|
+    t.integer "student_id"
+    t.integer "course_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "name"
